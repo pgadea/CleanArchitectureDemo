81d4d9be32fd6d14f234ec1986208beb27d6f89b
Pierre <pgadea@users.noreply.github.com>
2019-05-11 17:31:45 -0500

    Initial Commit of Infrastructure.Data Project. Update MVC for new UniversityDBContext

---
+++ CleanArchitecture.Infrastructure.Data/Context/UniversityDBContext.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitecture.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Infrastructure.Data.Context
+{
+    public class UniversityDBContext : DbContext
+    {
+        public UniversityDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
--- CleanArchitecture.MVC/Startup.cs
+++ CleanArchitecture.MVC/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.EntityFrameworkCore;
 using CleanArchitecture.MVC.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using CleanArchitecture.Infrastructure.Data.Context;
 
 namespace CleanArchitecture.MVC
 {
@@ -41,6 +42,11 @@ namespace CleanArchitecture.MVC
             services.AddDefaultIdentity<IdentityUser>()
                 .AddDefaultUI(UIFramework.Bootstrap4)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddDbContext<UniversityDBContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("UniversityIdentityDBConnection"));
+            });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
