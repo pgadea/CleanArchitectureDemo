a061848c444f84a44f03fc4fb00a914f3e272855
Pierre <pgadea@users.noreply.github.com>
2019-05-12 11:16:24 -0500

    Commit changes for Presentation Course Controller, Views, Styling, Authentication

---
+++ CleanArchitecture.MVC/Controllers/CourseController.cs
@@ -0,0 +1,27 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CleanArchitecture.Mvc.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            CourseViewModel model = _courseService.GetCourses();
+            return View(model);
+        }
+    }
+}
