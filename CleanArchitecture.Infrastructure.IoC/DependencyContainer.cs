995d0e0dd34169e5a07cc989d23e1ff3b469b06c
Pierre <pgadea@users.noreply.github.com>
2019-05-12 09:54:47 -0500

    Configured Presentation Layer for Dependency Container

---
+++ CleanArchitecture.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,17 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Application.Services;
+using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Infrastructure.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Infrastructure.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddScoped<ICourseService, CourseService>();
+            services.AddScoped<ICourseRepository, CourseRepository>();
+        }
+    }
+}
--- CleanArchitecture.MVC/Startup.cs
+++ CleanArchitecture.MVC/Startup.cs
@@ -14,6 +14,7 @@ using CleanArchitecture.MVC.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using CleanArchitecture.Infrastructure.Data.Context;
+using CleanArchitecture.Infrastructure.IoC;
 
 namespace CleanArchitecture.MVC
 {
@@ -49,6 +50,8 @@ namespace CleanArchitecture.MVC
             });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -78,6 +81,11 @@ namespace CleanArchitecture.MVC
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
     }
 }
