edb809a784cda565a78f66d063baeba489340b2d
Pierre <pgadea@users.noreply.github.com>
2019-06-02 10:18:16 -0500

    Courses Service Create Functionality, Updated CourseViewModel

--- CleanArchitecture.Application/Interfaces/ICourseService.cs
+++ CleanArchitecture.Application/Interfaces/ICourseService.cs
@@ -5,5 +5,6 @@ namespace CleanArchitecture.Application.Interfaces
     public interface ICourseService
     {
         CourseViewModel GetCourses();
+        void Create(CourseViewModel courseViewModel);
     }
 }
--- CleanArchitecture.Application/Services/CourseService.cs
+++ CleanArchitecture.Application/Services/CourseService.cs
@@ -1,5 +1,7 @@
 ﻿using CleanArchitecture.Application.Interfaces;
 using CleanArchitecture.Application.ViewModels;
+using CleanArchitecture.Domain.Commands;
+using CleanArchitecture.Domain.Core.Bus;
 using CleanArchitecture.Domain.Interfaces;
 
 namespace CleanArchitecture.Application.Services
@@ -7,10 +9,23 @@ namespace CleanArchitecture.Application.Services
     public class CourseService : ICourseService
     {
         private readonly ICourseRepository _courseRepository;
+        private readonly IMediatorHandler _bus;
 
-        public CourseService(ICourseRepository courseRepository)
+        public CourseService(ICourseRepository courseRepository, IMediatorHandler bus)
         {
             _courseRepository = courseRepository;
+            _bus = bus;
+        }
+
+        public void Create(CourseViewModel courseViewModel)
+        {
+            var createCourseCommand = new CreateCourseCommand(
+                courseViewModel.Name,
+                courseViewModel.Description,
+                courseViewModel.ImageUrl
+                );
+
+            _bus.SendCommand(createCourseCommand);
         }
 
         public CourseViewModel GetCourses()
