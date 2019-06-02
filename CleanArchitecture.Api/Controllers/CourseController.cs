bdf7c77d780c4bd8a21243dd6bf47b1206b49ce6
Pierre <pgadea@users.noreply.github.com>
2019-06-02 13:33:19 -0500

    Add Api project and Course Controller

---
+++ CleanArchitecture.Api/Controllers/CourseController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+       
+    }
+}
